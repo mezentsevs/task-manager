8410b2f5c6fb3cdaf3194c6d4d6be2b5de0a5c55
mezentsevs <mezentsevs@rambler.ru>
2026-07-11 21:55:28 +0400

    feat: add message components

---
+++ frontend/app/components/uikit/messages/ErrorMessage.vue
@@ -0,0 +1,13 @@
+<template>
+    <BaseMessage v-if="message" class="text-red-500">
+        {{ message }}
+    </BaseMessage>
+</template>
+
+<script setup lang="ts">
+import BaseMessage from '~/components/uikit/messages/BaseMessage.vue';
+
+defineProps<{
+    message: string;
+}>();
+</script>
