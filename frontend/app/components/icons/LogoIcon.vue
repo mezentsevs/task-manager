e7e308bcb85ebc5d0db93efda424e8d01be2b134
mezentsevs <mezentsevs@rambler.ru>
2026-07-11 22:47:15 +0400

    feat: add logo icon component

---
+++ frontend/app/components/icons/LogoIcon.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round">
+        <path d="M6 4h12" />
+        <path d="M12 4v16" />
+        <path d="M5 20v-16l3.5 8 3.5 -8v16" />
+    </svg>
+</template>
