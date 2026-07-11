ecb3ff231ace0fb03b20a2ade6a11e69bdc6df19
mezentsevs <mezentsevs@rambler.ru>
2026-07-11 22:17:21 +0400

    feat: add heading component

---
+++ frontend/app/components/uikit/headings/Heading.vue
@@ -0,0 +1,30 @@
+<template>
+    <component :is="heading" class="font-bold truncate text-gray-800 dark:text-gray-200">
+        <slot />
+    </component>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = withDefaults(
+    defineProps<{
+        level?: number;
+    }>(),
+    {
+        level: 1,
+    },
+);
+
+const heading = computed((): string => {
+    if (props.level < 1) {
+        return 'h1';
+    }
+
+    if (props.level > 6) {
+        return 'h6';
+    }
+
+    return `h${props.level}`;
+});
+</script>
