afc2233a824bd1c5aa786b335164b363f8d1c9c8
mezentsevs <mezentsevs@rambler.ru>
2026-07-11 22:09:01 +0400

    feat: add link components

---
+++ frontend/app/components/uikit/links/BaseRouterLink.vue
@@ -0,0 +1,15 @@
+<template>
+    <NuxtLink
+        :to
+        class="text-sm underline text-blue-500 hover:text-gray-700 active:text-gray-900 dark:text-gray-200 dark:hover:text-gray-100 dark:active:text-gray-300 transition duration-150 ease-in-out">
+        <slot />
+    </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import type { UikitRouterLinkTo } from '~/types/UikitTypes';
+
+defineProps<{
+    to: UikitRouterLinkTo;
+}>();
+</script>
---
+++ frontend/app/components/uikit/links/PrimaryRouterLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <BaseRouterLink :to class="text-blue-500 dark:text-gray-200">
+        <slot />
+    </BaseRouterLink>
+</template>
+
+<script setup lang="ts">
+import BaseRouterLink from '~/components/uikit/links/BaseRouterLink.vue';
+import type { UikitRouterLinkTo } from '~/types/UikitTypes';
+
+withDefaults(
+    defineProps<{
+        to?: UikitRouterLinkTo;
+    }>(),
+    {
+        to: '/',
+    },
+);
+</script>
