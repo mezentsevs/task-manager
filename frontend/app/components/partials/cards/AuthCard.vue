1328a7661d10a15fb7cf282e487015d1dfd325da
mezentsevs <mezentsevs@rambler.ru>
2026-07-11 22:34:22 +0400

    feat: add card components

---
+++ frontend/app/components/partials/cards/AuthCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <BaseCard class="w-80">
+        <template #base-logo>
+            <slot name="logo" />
+        </template>
+        <slot />
+    </BaseCard>
+</template>
+
+<script setup lang="ts">
+import BaseCard from '~/components/partials/cards/BaseCard.vue';
+</script>
