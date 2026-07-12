8e6c48280f266c96c83ca34de7479029db6fb166
mezentsevs <mezentsevs@rambler.ru>
2026-07-12 10:26:49 +0400

    feat: add ThemeToggle component with icons

---
+++ frontend/app/components/uikit/toggles/ThemeToggle.vue
@@ -0,0 +1,19 @@
+<template>
+    <button class="rounded-full bg-gray-200 p-2 dark:bg-gray-800" @click="toggleTheme">
+        <ThemeLightIcon v-if="isDark" class="h-6 w-6 text-gray-200" />
+        <ThemeDarkIcon v-else class="h-6 w-6 text-gray-800" />
+    </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ThemeDarkIcon from '~/components/icons/ThemeDarkIcon.vue';
+import ThemeLightIcon from '~/components/icons/ThemeLightIcon.vue';
+
+const isDark = ref<boolean>(false);
+
+const toggleTheme = (): void => {
+    isDark.value = !isDark.value;
+    document.documentElement.classList.toggle('dark', isDark.value);
+};
+</script>
