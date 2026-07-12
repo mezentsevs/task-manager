bc0e13ad36534f74d6525b70beb731ed4eecfc87
mezentsevs <mezentsevs@rambler.ru>
2026-07-12 17:04:03 +0400

    feat: add restore user info after page refresh

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -7,5 +7,16 @@
 
 <script setup lang="ts">
 import '~/assets/css/main.css';
+import { onMounted } from 'vue';
+import { retrieveToken } from '~/helpers/TokenHelper';
+import { useAuthStore } from '~/stores/auth';
 import AppHeader from '~/components/partials/AppHeader.vue';
+
+const authStore = useAuthStore();
+
+onMounted(async () => {
+    if (retrieveToken() && !authStore.user) {
+        await authStore.fetchMe();
+    }
+});
 </script>
