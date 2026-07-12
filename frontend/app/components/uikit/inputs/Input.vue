4c4863e4bfae4d4c077c6858d76ee90b3c243b2d
mezentsevs <mezentsevs@rambler.ru>
2026-07-12 14:55:45 +0400

    feat: update input padding and colors

--- frontend/app/components/uikit/inputs/Input.vue
+++ frontend/app/components/uikit/inputs/Input.vue
@@ -1,7 +1,7 @@
 <template>
     <input
         v-model="model"
-        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600 rounded-md shadow-sm" />
+        class="px-3 py-2 border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-300 bg-white dark:bg-gray-900 focus:border-blue-500 dark:focus:border-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600 rounded-md shadow-sm" />
 </template>
 
 <script setup lang="ts">
